254fad1e8c750e5abb8172ad42763e7cfdbbe66f
Bolero Xu <22206837+Esquire6048@users.noreply.github.com>
2024-08-29 11:04:11 +0900

    fix: modified category keyword

--- src/RelatedProductAndRecipe.jsx
+++ src/RelatedProductAndRecipe.jsx
@@ -2,14 +2,14 @@ import ProductList from "./ProductList.jsx";
 import RecipeList from "./RecipeList.jsx";
 
 const RelatedProductAndRecipe = ({ category, relatedKeyword }) => {
-    if (category === "product") {
+    if (category === "Product") {
         return (
             <div>
                 <h2 className="products-relatedto-rakuten">{relatedKeyword}をお探しですか？</h2>
                 <ProductList productKeyword={relatedKeyword}/>
             </div>
         )
-    } else if (category === "recipe") {
+    } else if (category === "Recipe") {
         return (<div>
             <h2 className="products-relatedto-rakuten">賞味期限が近い備蓄食料をおいしく調理</h2>
             <RecipeList recipeID={relatedKeyword}/>
