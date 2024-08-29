f65c717ca78e80d747fe88d1f6657e57609fcfb3
RYUHEIPC\ryuho <longpingxigu@gmail.com>
2024-08-29 15:02:33 +0900

    Homeの配置修正

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,12 +9,14 @@ const Home = () => {
 
   return (
     <div className="home">
-      <img src={exampleImage} alt="Example" className="example-image"/>
-      <h1>正解数だけ<span>楽天ポイント</span>獲得！楽天防災クイズ！</h1>
-      <button onClick={navigateToQuiz} className="regular-route">クイズにチャレンジ</button>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.rakuten.co.jp" >
-        <button>楽天市場に戻る</button>
-      </a>
+      <img src={exampleImage} alt="Example" className="example-image" width="500" height="500" />
+      <div className="home-right">
+        <h1>正解数だけ<span>楽天ポイント</span>獲得！</h1>
+        <button onClick={navigateToQuiz} className="regular-route">クイズにチャレンジ</button>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.rakuten.co.jp" >
+          <button>楽天市場に戻る</button>
+        </a>
+      </div>
     </div>
   );
 }
