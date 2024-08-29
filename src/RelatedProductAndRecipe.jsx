6e4a24e256be72a3d32b0bb876a7d8ec05a3401d
Bolero Xu <22206837+Esquire6048@users.noreply.github.com>
2024-08-29 10:58:53 +0900

    fix: modified ProductList.css

--- src/RelatedProductAndRecipe.jsx
+++ src/RelatedProductAndRecipe.jsx
@@ -5,13 +5,13 @@ const RelatedProductAndRecipe = ({ category, relatedKeyword }) => {
     if (category === "product") {
         return (
             <div>
-                <h2>{relatedKeyword}をお探しですか？</h2>
+                <h2 className="products-relatedto-rakuten">{relatedKeyword}をお探しですか？</h2>
                 <ProductList productKeyword={relatedKeyword}/>
             </div>
         )
     } else if (category === "recipe") {
         return (<div>
-            <h2>賞味期限が近い備蓄食料をおいしく調理</h2>
+            <h2 className="products-relatedto-rakuten">賞味期限が近い備蓄食料をおいしく調理</h2>
             <RecipeList recipeID={relatedKeyword}/>
         </div>)
     } else {
